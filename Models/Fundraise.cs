806e2a6e117c517018170d91f390d7d665a6e3f1
jaqubm <jaqobgm@gmail.com>
2024-11-23 22:27:20 +0100

    Fundraise preparation

--- Models/Fundraise.cs
+++ Models/Fundraise.cs
@@ -3,39 +3,48 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace schoolMoney_backend.Models;
 
-public class Fundraise(string title, string description, decimal goalAmount, DateTime startDate, DateTime endDate)
+public class Fundraise
 {
     [Key]
     [MaxLength(50)]
-    public string FundraiseId { get; set; } = Guid.NewGuid().ToString();
+    public string FundraiseId { get; set; }
     
     [Required]
     [MaxLength(255)]
-    public string Title { get; set; } = title;
+    public string Title { get; set; }
     
     [Required]
-    public string Description { get; set; } = description;
+    public string Description { get; set; }
+
+    [Required] 
+    public decimal GoalAmount { get; set; }
+
+    [Required]
+    public DateTime StartDate { get; set; }
     
     [Required]
-    public decimal GoalAmount { get; set; } = goalAmount;
-    
-    [Required]
-    public DateTime StartDate { get; set; } = startDate;
-    
-    [Required]
-    public DateTime EndDate { get; set; } = endDate;
+    public DateTime EndDate { get; set; }
 
     [Required]
     [MaxLength(50)]
     [ForeignKey("Class")]
-    public string? ClassId { get; set; }
+    public string ClassId { get; set; }
     public virtual Class? Class { get; set; }
 
     [Required]
     [MaxLength(12)]
     [ForeignKey("Account")]
-    public string? AccountNumber { get; set; }
+    public string AccountNumber { get; set; }
     public virtual Account? Account { get; set; }
 
     public virtual ICollection<Transaction>? Transactions { get; set; } = [];
+
+    public Fundraise()
+    {
+        FundraiseId = Guid.NewGuid().ToString();
+        Title ??= string.Empty;
+        Description ??= string.Empty;
+        ClassId ??= string.Empty;
+        AccountNumber ??= string.Empty;
+    }
 }
